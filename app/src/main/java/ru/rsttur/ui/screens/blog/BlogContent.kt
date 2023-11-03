3a34ad9c943e8af74116ebf975713d17f135a9ff
pojiloyflex <georgeklass@gmail.com>
2023-11-03 21:49:53 +0300

    RSTTUR-4 add app icon and floating button in blog page

--- app/src/main/java/ru/rsttur/ui/screens/blog/BlogContent.kt
+++ app/src/main/java/ru/rsttur/ui/screens/blog/BlogContent.kt
@@ -1,18 +1,25 @@
 package ru.rsttur.ui.screens.blog
 
 import BaseImage
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,24 +36,37 @@ import ru.rsttur.ui.components.ErrorContent
 import ru.rsttur.ui.components.LoadingContent
 import ru.rsttur.utils.ScreenState
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BlogContent(navController: NavController, title: String, id: Long) {
+fun BlogContent(navController: NavController, id: Long) {
     val blogViewModel = hiltViewModel<BlogViewModel>()
     val state = blogViewModel.state.collectAsState()
     blogViewModel.getBlog(DEFAULT_ID, id)
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally
+    Box(modifier = Modifier.fillMaxSize()) {
+        Surface(
+            modifier = Modifier.fillMaxSize()
         ) {
-            Content(state.value.screenState == ScreenState.UPLOADED, state.value.blog)
-            LoadingContent(state.value.screenState == ScreenState.LOADING)
-            ErrorContent(state.value.screenState == ScreenState.ERROR)
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Content(state.value.screenState == ScreenState.UPLOADED, state.value.blog)
+                LoadingContent(state.value.screenState == ScreenState.LOADING)
+                ErrorContent(state.value.screenState == ScreenState.ERROR)
+            }
+        }
+        FloatingActionButton(
+            onClick = blogViewModel::onNavigateToHomePage,
+            modifier = Modifier.align(Alignment.TopStart).padding(20.dp).size(40.dp),
+            shape = CircleShape
+        ) {
+            Icon(Icons.Default.ArrowBack, contentDescription = "Add")
+        }
+        LaunchedEffect(key1 = blogViewModel.effect) {
+            blogViewModel.effect.collect { effect ->
+                if (effect is BlogEffect.NavigateToHomePage) navController.navigateUp()
+            }
         }
     }
 }
@@ -71,7 +91,7 @@ fun Content(isDisplayed: Boolean, blog: Blog?) {
         )
         Text(
             modifier = Modifier
-                .padding(bottom = 6.dp),
+                .padding(vertical = 3.dp , horizontal = 12.dp),
             text = blog.title,
             fontWeight = FontWeight.ExtraBold,
             overflow = TextOverflow.Ellipsis,
@@ -80,7 +100,9 @@ fun Content(isDisplayed: Boolean, blog: Blog?) {
             textAlign = TextAlign.Justify
         )
         Text(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
             text = blog.content,
             fontWeight = FontWeight.Light,
             overflow = TextOverflow.Ellipsis,
